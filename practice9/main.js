d49d601d46c32df4fe35084ec234236a775f786a
ishige-shogo <shigeshigeishige@gmail.com>
2021-02-07 16:20:42 +0900

    [pra]

--- practice9/main.js
+++ practice9/main.js
@@ -1,7 +1,18 @@
-new Vue({
-    el: '#app',
+var vm1 = new Vue({
+    el: '#app1',
     data: {
-        ok: true
+        message: "インスタンス1"
     }
-    
+})
+
+var vm2 = new Vue({
+    el: '#app2',
+    data: {
+        message: "インスタンス2"
+    },
+    methods: {
+        changeMessage1: function(){
+            vm1.message = "インスタンス2によって変わりました。"
+        }
+    }
 })
