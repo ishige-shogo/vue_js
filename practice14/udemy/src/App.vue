507cb531557b5f1e387a16d9b3f478396fa74492
ishige-shogo <shigeshigeishige@gmail.com>
2021-02-19 10:47:34 +0900

    new_vue_create

---
+++ practice14/udemy/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切替</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      show: true
+    };
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter{
+
+}
+.fade-enter-active{
+
+}
+.fade-enter-to{
+
+}
+.fade-leave{
+
+}
+.fade-leave-active{
+
+}
+.fade-leave-to{
+  
+}
+
+.main{
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
